0535ffa78da4d492cd358eb13d69a0592d596ea7
Wahyu Aprilliandhika <wahyuandhika910@gmail.com>
2022-12-31 22:05:59 +0700

    update

--- finalproject.ino
+++ finalproject.ino
@@ -1,20 +1,20 @@
 #define BLYNK_TEMPLATE_ID "TMPLn9DgB2E2"
 #define BLYNK_DEVICE_NAME "Quickstart Device"
 #define BLYNK_AUTH_TOKEN "o4tpnRdfMPYxI2MyQncAxKU4kOPDF2Q6"
-char ssid [] = "vivo 1820";
-char pass [] = "12345678";
+char ssid [] = "Durio Indigo";
+char pass [] = "indigo123";
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <Servo.h>
 char auth[] = BLYNK_AUTH_TOKEN;
 BlynkTimer timer;
 
 float cahaya ;
-  int hujan;
-  int posisi = 0;
-
-#include <Servo.h>
+int hujan;
+int posisi = 0;
+int manual;
 
 Servo myservo;  // create servo object to control a servo
 
@@ -22,38 +22,36 @@ Servo myservo;  // create servo object to control a servo
 #include <BH1750_WE.h>
 #define BH1750_ADDRESS 0x23
 BH1750_WE myBH1750 = BH1750_WE(BH1750_ADDRESS); 
-
+ 
 
 void myTimerEvent()
 {
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
   Blynk.virtualWrite(V1, posisi);
-    Blynk.virtualWrite(V2, hujan);
-    Blynk.virtualWrite(V4, cahaya);
+  Blynk.virtualWrite(V2, hujan);
+  Blynk.virtualWrite(V4, cahaya);
 
 }
 
-
-
 void setup() {
   // put your setup code here, to run once:
-   myservo.attach(12);  // attaches the servo on GIO2 to the servo object
-     Wire.begin();
+  myservo.attach(12);  // inisialisasi General Purpose Input Output(GPIO) pada pinD6
+  Wire.begin();
   myBH1750.init();//sensor cahaya
   Serial.begin(9600);
-  myservo.write(0);//pin d6
+  myservo.write(0);//mengatur pergerakan servo berdasarkan derajat
   Blynk.begin(auth, ssid, pass);
   timer.setInterval(1000L, myTimerEvent);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   Blynk.run();
   timer.run();
-   cahaya = myBH1750.getLux(); //pin scl sdh
-   hujan = (analogRead(A0)); //pin analog
+  cahaya = myBH1750.getLux(); //pin scl sdh
+  hujan = (analogRead(A0)); //pin analog
   
   Serial.print(cahaya);
   Serial.print("  ");
@@ -61,49 +59,24 @@ void loop() {
   Serial.print("  ");
   Serial.println(posisi);
 
-
-  
-  
-  if (cahaya > 10 && hujan > 1000 && posisi == 0)
-  {//cerah tidak hujan dan posisi diluar
+  if (cahaya > 10 && hujan > 1000 && posisi == 0 && manual == 0)
+  {//cerah tidak hujan dan posisi jemuran di dalam maka jemuran akan keluar
     myservo.write(180);
     posisi = 1;
   }
-  else if(cahaya < 10 && hujan > 1000 && posisi == 0)
-  {//gelap tidak hujan posisi dalam
-   myservo.write(0);
-   posisi = 0;
-  }
-  else if (cahaya > 10 && hujan < 1000 && posisi == 0)
-  {//cerah tapi hujan posisi dalam
+  else if (cahaya < 10 && hujan > 1000 && posisi == 1 && manual == 1)
+  {//gelap dan tidak hujan dan posisi jemuran sedang diluar maka jemuran akan masuk
     myservo.write(0);
     posisi = 0;
   }
-  else if (cahaya < 10 && hujan < 1000 && posisi == 0)
-  {//gelap dan hujan posisi dalam
-    myservo.write(0);
+  else if (cahaya > 10 && hujan < 1000 && posisi == 1 && manual == 0)
+  {// cerah tapi hujan posisi dan posisi jemuran sedang diluar maka jemuran akan masuk
+    myservo.write(0);    
     posisi = 0;
-  }
-  else if (cahaya > 10 && hujan > 1000 && posisi == 1)
-  {//cerah dan tidak hujan posisi diluar
-    myservo.write(180);
-    posisi = 1;
-  }
-  else if (cahaya < 10 && hujan > 1000 && posisi == 1)
-  {//gelap dan tidak hujan posisi diluar
-    myservo.write(0);
-    posisi = 0;
-  }
-  else if (cahaya > 10 && hujan < 1000 && posisi == 1)
-  {//cerah dan hujan diluar
-    myservo.write(0);
-    posisi = 0;
-  }
-  else if (cahaya < 10 && hujan < 1000 && posisi == 1)
-  {//gelap dan hujan posisi masuk
-    myservo.write(0);
-    posisi = 0;
-  }
+  } 
+}
 
-  
+BLYNK_WRITE(V1)
+{
+  manual =  param.asInt(); 
 }
